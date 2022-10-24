7becd7cbede03e7df42ef8d083e066cc146d5939
JujenioP <110836213+MaxAbelMamani@users.noreply.github.com>
2022-10-24 16:47:27 -0300

    Poniendo Imagenes y configuracion del game

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Phaser from "phaser";
+import { useEffect, useState } from "react";
+//Importando escenas
+import Game from "../Escenas/Game.jsx";
+import GameOver from "../Escenas/GameOver.jsx";
+import Congratulations from "../Escenas/Congratulations";
+import GameStart from "../Escenas/GameStart.jsx";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [listo, setListo] = useState(false);
+    useEffect(()=>{
+        //ancho del juego
+        const width = 800;
+        //alto del juego
+        const height = 480;
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+        const config = {
+            type: Phaser.AUTO,
+            width: width,
+            height: height,
+            physics: {
+                default: "arcade",
+                arcade: {
+                    debug:false
+                },
+            },
+            scene: [GameStart ,Game, GameOver, Congratulations],
+        };
+        const game = new Phaser.Game(config);
+        game.events.on("LISTO", setListo);
+        return() => {
+            setListo(false);
+            game.destroy(true);
+        }
+
+    },[listo]);
 }
 
 export default App
