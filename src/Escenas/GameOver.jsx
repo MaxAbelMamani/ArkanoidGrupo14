563740260d3838872afac045b04d22acd18a692e
MatiasGrrrOrtega <mat-1998-ort@hotmail.com>
2022-10-24 17:41:30 -0300

    Cambios en las escenas y comentarios

--- src/Escenas/GameOver.jsx
+++ src/Escenas/GameOver.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BotonReiniciar } from '../Funciones/BotonReiniciar';
+import { BotonReiniciar } from '../Clases/BotonReiniciar.js';
 import gameOver from '../assets/sprites/gameOver.png'
 
 class GameOver extends (React.Component, Phaser.Scene) {
