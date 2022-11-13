291cbda136fa29f2aee39cff1f783627dce1aa7a
Agmor03 <morales.agmaar03@gmail.com>
2022-11-13 20:34:01 -0300

    Agregando sonidos al juego

--- src/Escenas/Nivel1.jsx
+++ src/Escenas/Nivel1.jsx
@@ -4,6 +4,13 @@ import Phaser from 'phaser'
 //Importando Imagenes
 import background from '../assets/sprites/background.jpg'
 
+//Importando Sonidos
+import BlockSound from '../assets/sounds/block.mp3'
+import BoingSound from '../assets/sounds/Boing.mp3'
+import FailSound from '../assets/sounds/fail.mp3'
+import StartSound from '../assets/sounds/start.wav'
+import BackgroundSound from '../assets/sounds/backgroundsound.mp3'
+
 //Importando componentes
 import marcadorGame from '../Components/marcadorGame.jsx'
 import vidasGame from '../Components/vidasGame.jsx'
@@ -29,6 +36,11 @@ class Nivel1 extends (React.Component, Phaser.Scene) {
 
     preload(){
         this.load.image('background',background);
+        this.load.audio('block', BlockSound);
+        this.load.audio('boing', BoingSound);
+        this.load.audio('fail', FailSound);
+        this.load.audio('start', StartSound);
+        this.load.audio('bgSound', BackgroundSound);
         this.paleta.preload();
         this.bola.preload();
         this.bloques.preload();
@@ -40,6 +52,7 @@ class Nivel1 extends (React.Component, Phaser.Scene) {
         //El valor false representa a la pared inferior la cual esta no tendra colision con el onbjeto bola
         this.physics.world.setBoundsCollision(true, true, true, false);
         this.crearFondo();
+        this.crearSonidos();
         this.marcador.create();
         this.vida.create();
         this.bloques.crearBloques(this.nivel);
@@ -52,18 +65,31 @@ class Nivel1 extends (React.Component, Phaser.Scene) {
         this.physics.add.collider(this.bola.pelotaGame, this.bloques.conjuntoBloques, this.colisionBolaBloque, null, this);
 
         this.cursors = this.input.keyboard.createCursorKeys();
-        this.precionarTeclaEspacio = true; 
+        this.precionarTeclaEspacio = true;
+        this.startSound.play();
     }
 
     crearFondo(){
         this.add.image(400,300,'background');
     }
 
+    crearSonidos(){
+        this.blockSound = this.sound.add('block');
+        this.boingSound = this.sound.add('boing');
+        this.failSound = this.sound.add('fail');
+        this.startSound = this.sound.add('start');
+        this.backgroundSound = this.sound.add('bgSound');
+        this.backgroundSound.loop  = true;
+        this.backgroundSound.play();
+    }
+
     colisionBolaBloque(bola, bloque){
         bloque.disableBody(true, true);
         this.marcador.incremenarPuntaje(10);
+        this.blockSound.play();
         if (this.bloques.conjuntoBloques.countActive() == 0) {
             this.scene.start('nivel2');
+            this.backgroundSound.pause();
         }
     }
 
@@ -71,6 +97,7 @@ class Nivel1 extends (React.Component, Phaser.Scene) {
         this.marcador.incremenarPuntaje(1);
         let impactoRelativo = bola.x - paleta.x;
         console.log(impactoRelativo);
+        this.boingSound.play();
         if (impactoRelativo < 0.1 && impactoRelativo > -0.1) {
             bola.setVelocityX(Phaser.Math.Between(-10,10));
         }else{
@@ -102,6 +129,7 @@ class Nivel1 extends (React.Component, Phaser.Scene) {
             this.bola.pelotaGame.y = this.paleta.paletaPlayer.y - 40;
             this.bola.pelotaGame.setVelocity(0);
             this.bola.pelotaGame.setData('bolaPegada', true);
+            this.failSound.play();
             this.precionarTeclaEspacio = true;
         }
 
@@ -113,15 +141,18 @@ class Nivel1 extends (React.Component, Phaser.Scene) {
             this.precionarTeclaEspacio = false;
             this.bola.pelotaGame.setVelocity(-75,-300);
             this.bola.pelotaGame.setData('bolaPegada', false);
+            this.boingSound.play();
         }
     }
 
     mostrarGameOver(){
         this.scene.start('gameOver');
+        this.backgroundSound.stop();
     }
 
     mostrarCongratulations(){
         this.scene.start('congratulations');
+        this.backgroundSound.stop();
     }
 }
 
